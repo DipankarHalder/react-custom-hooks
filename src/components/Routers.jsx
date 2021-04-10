b8339a79204a05266fdacac081fa22f8aadd06a8
DipankarHalder <dipankar-halder@outlook.com>
2021-04-10 15:44:00 +0530

    added addEventListener

--- src/components/Routers.jsx
+++ src/components/Routers.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Fetch from './pages/Fetch';
+import EventListener from './pages/EventListener';
 
 export default function Routers() {
   return (
@@ -9,10 +10,12 @@ export default function Routers() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/useFetch" component={Fetch} />
+        <Route exact path="/useEventListener" component={EventListener} />
       </Switch>
       <ul className="app-navigation">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/useFetch">useFetch</Link></li>
+        <li><Link to="/useEventListener">useEventListener</Link></li>
       </ul>
     </BrowserRouter>
   )
---
+++ src/components/hooks/useEventListener.js
@@ -0,0 +1,16 @@
+import { useRef, useEffect } from 'react'
+
+export default function useEventListener(eventType = '', listener = () => null, target = window, options = null) {
+  const savedListener = useRef();
+  useEffect(() => { savedListener.current = listener; }, [listener])
+
+  useEffect(() => {
+    if(!target.addEventListener) return;
+    
+    const eventListener = event => savedListener.current(event);
+    target.addEventListener(eventType, eventListener, options )
+
+    return () => { target.removeEventListener(eventType, eventListener, options ) }
+  }, [eventType, target, options]);
+}
+ 
